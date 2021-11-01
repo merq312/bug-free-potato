d238682b3de3c57eeb04a776bf780f5280b22313
Chamila Gunasena <merc312@gmail.com>
2021-11-01 18:18:07 -0400

    Add tests for basic rendering

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import App from "./App"
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe("Homepage", () => {
+  test("App contains main section", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+    expect(screen.getByRole("main")).toBeInTheDocument()
+  })
+
+  test("App containers header", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+    expect(screen.getByRole("banner")).toBeInTheDocument()
+  })
+})
