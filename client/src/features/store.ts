aa7e9011893583731af87fd08d261bdd463b2d49
merq312 <merc312@gmail.com>
2022-01-25 00:21:37 -0500

    remove example

--- client/src/features/store.ts
+++ client/src/features/store.ts
@@ -1,6 +1,5 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 // import { logger } from "redux-logger"
-import counterReducer from './counter/counterSlice'
 import messageReducer from './message/messageSlice'
 import userReducer from './user/userSlice'
 import roomReducer from './room/roomSlice'
@@ -8,7 +7,6 @@ import { createSocketMiddleware } from './middleware/socket.middleware'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     messages: messageReducer,
     user: userReducer,
     rooms: roomReducer,
